2491d001dfa10443257375519845567eeb475604
ndescalzi <ndescalzi@gmail.com>
2021-06-29 11:33:39 -0300

    Primer test

---
+++ funciones.py
@@ -0,0 +1,84 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
+
+def selector(chrome, selector, elemento, uso, palabras=""):
+    espera = WebDriverWait(chrome, 15)
+    a = None
+    try:
+        if selector == "id" and uso == "click":
+            a = espera.until(EC.presence_of_element_located((By.ID, elemento)))
+            a.click()
+        elif selector == "id" and uso == "escritura":
+            a = espera.until(EC.presence_of_element_located((By.ID, elemento)))
+            a.send_keys(palabras)
+        elif selector == "id" and uso == "borrar":
+            a = espera.until(EC.presence_of_all_elements_located((By.ID, elemento)))
+            chrome.find_element_by_id(elemento).clear()
+        elif selector == "id" and uso == "texto":
+            a = espera.until(EC.presence_of_element_located((By.ID, elemento)))
+            a.text
+        elif selector == "id" and uso == "escritura y ENTER":
+            a = espera.until(EC.presence_of_element_located((By.ID, elemento)))
+            a.send_keys(palabras + Keys.ENTER)
+        elif selector == "xpath" and uso == "click":
+            a = espera.until(EC.element_to_be_clickable((By.XPATH, elemento)))
+            a.click()
+        elif selector == "xpath" and uso == "escritura":
+            a = espera.until(EC.presence_of_element_located((By.XPATH, elemento)))
+            a.send_keys(palabras)
+        elif selector == "css" and uso == "click":
+            a = espera.until(EC.element_to_be_clickable((By.CSS_SELECTOR, elemento)))
+            a.click()
+        elif selector == "css" and uso == "escritura":
+            a = espera.until(EC.element_to_be_clickable((By.CSS_SELECTOR, elemento)))
+            a.send_keys(palabras)
+        elif selector == "css" and uso == "texto":
+            a = espera.until(EC.element_to_be_clickable((By.CSS_SELECTOR, elemento)))
+            a.text
+    except Exception as e:
+        if selector == "id" and uso == "click":
+            a = espera.until(EC.visibility_of_element_located((By.ID, elemento)))
+            a.click()
+        elif selector == "id" and uso == "escritura":
+            a = espera.until(EC.visibility_of_element_located((By.ID, elemento)))
+            a.send_keys(palabras)
+        elif selector == "id" and uso == "borrar":
+            a = espera.until(EC.visibility_of_element_located((By.ID, elemento)))
+            a.clear()
+        elif selector == "id" and uso == "texto":
+            a = espera.until(EC.visibility_of_element_located((By.ID, elemento)))
+            a.text
+        elif selector == "xpath" and uso == "click":
+            a = espera.until(EC.visibility_of_element_located((By.XPATH, elemento)))
+            a.click()
+        elif selector == "xpath" and uso == "escritura":
+            a = espera.until(EC.visibility_of_element_located((By.XPATH, elemento)))
+            a.send_keys(palabras)
+        elif selector == "css" and uso == "click":
+            a = espera.until(EC.visibility_of_element_located((By.CSS_SELECTOR, elemento)))
+            a.click()
+        elif selector == "css" and uso == "escritura":
+            a = espera.until(EC.visibility_of_element_located((By.CSS_SELECTOR, elemento)))
+            a.send_keys(palabras)
+        elif selector == "css" and uso == "texto":
+            a = espera.until(EC.visibility_of_element_located((By.CSS_SELECTOR, elemento)))
+            a.text
+    return a
+
+
+def search_number_string(String):
+    # Esta funcion recorre un string y retorna los numeros que hay en ella.
+    index_list = []
+    del index_list[:]
+    for i, x in enumerate(String):
+        if x.isdigit() == True:
+            index_list.append(i)
+    start = index_list[0]
+    end = index_list[-1] + 1
+    number = String[start:end]
+    return number
+
---
+++ main.py
@@ -0,0 +1,34 @@
+import unittest
+from selenium import webdriver
+import funciones
+
+
+class SearchCase(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("chromedriver.exe")
+
+    def test_cantidad_remeras(self):
+        driver = self.driver
+        driver.get("http://automationpractice.com/index.php")
+        funciones.selector(driver, "id", "search_query_top", "escritura y ENTER", "remeras")
+        funciones.selector(driver, " xpath", '//*[@id="searchbox"]/button/span', "click")
+        resultado = driver.find_element_by_css_selector("div > h1> span[class='heading-counter']").text
+        resultado = funciones.search_number_string(resultado)
+        self.assertEqual(resultado, "0",msg="Se encontraron "+ resultado+" articulos.")
+
+    def test_cantidad_t_shirt(self):
+        driver = self.driver
+        driver.get("http://automationpractice.com/index.php")
+        funciones.selector(driver, "id", "search_query_top", "escritura y ENTER", "t-shirt")
+        funciones.selector(driver, " xpath", '//*[@id="searchbox"]/button/span', "click")
+        resultado = driver.find_element_by_css_selector("div > h1> span[class='heading-counter']").text
+        resultado = funciones.search_number_string(resultado)
+        self.assertEqual(resultado, "0",msg="Se encontraron "+ resultado+" articulos.")
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
